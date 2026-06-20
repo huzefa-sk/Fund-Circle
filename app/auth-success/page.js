f83e8a0b4884f440c9509b1c63076129482a3825
huzefa-sk <shaikhhuzefa3794@gmail.com>
2026-06-20 15:34:39 +0530

    dashboard testing rediredt testing 4

--- app/auth-success/page.js
+++ app/auth-success/page.js
@@ -4,19 +4,22 @@ import { useEffect } from "react";
 
 export default function AuthSuccess() {
   useEffect(() => {
+    console.log("opener:", window.opener);
+
     try {
       if (window.opener && !window.opener.closed) {
+        console.log("popup flow");
         window.opener.location.href = "/dashboard";
-
-        setTimeout(() => {
-          window.close();
-        }, 500);
+        window.close();
+      } else {
+        console.log("normal flow");
+        window.location.replace("/dashboard");
       }
     } catch (error) {
+      console.log(error);
       window.location.replace("/dashboard");
     }
   }, []);
-
   return (
     <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px', color: 'white' }}>
       <p>Login successful! Redirecting...</p>
